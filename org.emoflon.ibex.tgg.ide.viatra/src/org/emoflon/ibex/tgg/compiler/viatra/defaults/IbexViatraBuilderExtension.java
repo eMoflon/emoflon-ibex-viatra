552f7b2a0b55a3918f1fc215d44a6c3aad350ac1
Julian Barthel <j.barthel@gmx.de>
2020-05-13 18:03:40 +0200

    Updated files generator for ViatraRegistrationHelper

--- org.emoflon.ibex.tgg.ide.viatra/src/org/emoflon/ibex/tgg/compiler/viatra/defaults/IbexViatraBuilderExtension.java
+++ org.emoflon.ibex.tgg.ide.viatra/src/org/emoflon/ibex/tgg/compiler/viatra/defaults/IbexViatraBuilderExtension.java
@@ -2,12 +2,18 @@ package org.emoflon.ibex.tgg.compiler.viatra.defaults;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.apache.log4j.Logger;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.emf.ecore.EPackage;
 import org.emoflon.ibex.common.project.ManifestHelper;
+import org.emoflon.ibex.tgg.compiler.viatra.defaults.ViatraFilesGenerator;
 import org.emoflon.ibex.tgg.ide.admin.BuilderExtension;
 import org.emoflon.ibex.tgg.ide.admin.IbexTGGBuilder;
 import org.moflon.core.plugins.manifest.ManifestFileUpdater;
@@ -16,12 +22,52 @@ import org.moflon.tgg.mosl.tgg.TripleGraphGrammarFile;
 
 public class IbexViatraBuilderExtension implements BuilderExtension {
 
-	private Logger logger = Logger.getLogger(IbexViatraBuilderExtension.class);
+	private static Logger logger = Logger.getLogger(IbexViatraBuilderExtension.class);
 	
 	@Override
 	public void run(IbexTGGBuilder builder, TripleGraphGrammarFile editorModel, TripleGraphGrammarFile flattenedEditorModel) {
-		try {
-			builder.createDefaultDebugRunFile("MODELGEN_Debug_App", (projectName, fileName) 
+		
+		// create the actual project path
+				EPackage srcPkg = flattenedEditorModel.getSchema().getSourceTypes().get(0);
+				EPackage trgPkg = flattenedEditorModel.getSchema().getTargetTypes().get(0);
+				EPackage corrPkg = flattenedEditorModel.eClass().getEPackage();
+				try {
+					if(srcPkg == null || trgPkg == null || corrPkg == null) {
+						throw new RuntimeException("Could not get flattened trg or src model from editor model.");
+					}
+				} catch (Exception e) {
+					LogUtils.error(logger, e); 
+					return;
+				}
+				
+				String srcModel = srcPkg.getName();
+				String trgModel = trgPkg.getName();
+				
+				IProject srcProject = getProjectInWorkspace(srcModel, builder.getProject().getWorkspace());
+				IProject trgProject = getProjectInWorkspace(trgModel, builder.getProject().getWorkspace());
+				
+				String srcPkgName = null;
+				String trgPkgName = null;
+				
+				if(srcProject != null) {
+					srcPkgName = getRootPackageName(srcProject);
+				}
+				if(trgProject != null) {
+					trgPkgName = getRootPackageName(trgProject);
+				}
+				
+				LogUtils.info(logger, "Building missing app stubs...");
+				try {
+					generateRegHelper(builder, srcProject, trgProject, srcPkgName, trgPkgName);
+					generateDefaultStubs(builder, editorModel, flattenedEditorModel);
+				}catch(Exception e) {
+					LogUtils.error(logger, e);
+				}
+		updateManifest(builder.getProject());
+	}
+	
+	public void generateDefaultStubs(IbexTGGBuilder builder, TripleGraphGrammarFile editorModel, TripleGraphGrammarFile flattenedEditorModel) throws CoreException {
+		builder.createDefaultDebugRunFile("MODELGEN_Debug_App", (projectName, fileName) 
 				-> ViatraFilesGenerator.generateModelGenDebugFile(projectName, fileName));
 			builder.createDefaultRunFile("MODELGEN_App", (projectName, fileName) 
 					-> ViatraFilesGenerator.generateModelGenFile(projectName, fileName));
@@ -45,11 +91,84 @@ public class IbexViatraBuilderExtension implements BuilderExtension {
 					-> ViatraFilesGenerator.generateDefaultRegHelperFile(projectName));
 			builder.createDefaultConfigFile("ViatraRegistrationHelper", (projectName, fileName)
 					-> ViatraFilesGenerator.generateRegHelperFile(projectName, editorModel));
-		} catch (CoreException e) {
-			LogUtils.error(logger, e);
+	}
+	
+	public void generateRegHelper(IbexTGGBuilder builder, IProject srcProject, IProject trgProject, String srcPkg, String trgPkg) throws Exception {
+		String input_srcProject = srcProject == null ? "<<SRC_Project>>" : srcProject.getName();
+		String input_trgProject = trgProject == null ? "<<TRG_Project>>" : trgProject.getName();
+		String input_srcPackage = srcPkg == null ? "<<SRC_Package>>" : srcPkg;
+		String input_trgPackage = trgPkg == null ? "<<TRG_Package>>" : trgPkg;
+		
+		builder.createDefaultConfigFile(ViatraFilesGenerator.REGISTRATION_HELPER, (projectName, fileName)
+				-> ViatraFilesGenerator.generateRegHelperFile(projectName, input_srcProject, input_trgProject, input_srcPackage, input_trgPackage));
+	}
+	
+	private static IProject getProjectInWorkspace(String modelName, IWorkspace workspace) {
+		IProject[] projects = workspace.getRoot().getProjects();
+		for(IProject project : projects) {
+			if(project.getName().toLowerCase().equals(modelName.toLowerCase())) {
+				return project;
+			}
+		}
+		LogUtils.info(logger, "The project belonging to model "+modelName+" could not be found in the workspace.");
+		return null;
+	}
+	
+	private static String getRootPackageName(IProject project) {
+		String upperPkgName = project.getName();
+		String firstLower = project.getName().substring(0, 1).toLowerCase()+project.getName().substring(1);
+		String lowerPkgName = project.getName().toLowerCase();
+		
+		IPath projectPath = project.getLocation().makeAbsolute();
+		Path srcPath = Paths.get(projectPath.toPortableString()+"/src");
+		File srcFolder = srcPath.toFile();
+		if(srcFolder.exists() && srcFolder.isDirectory()) {
+			for(String fName : srcFolder.list()) {
+				if(fName.equals(upperPkgName)) {
+					return upperPkgName;
+				}
+				if(fName.equals(firstLower)) {
+					return firstLower;
+				}
+				if(fName.equals(lowerPkgName)) {
+					return lowerPkgName;
+				}
+			}	
+		}
+		srcPath = Paths.get(projectPath.toPortableString()+"/src-gen");
+		srcFolder = srcPath.toFile();
+		if(srcFolder.exists() && srcFolder.isDirectory()) {
+			for(String fName : srcFolder.list()) {
+				if(fName.equals(upperPkgName)) {
+					return upperPkgName;
+				}
+				if(fName.equals(firstLower)) {
+					return firstLower;
+				}
+				if(fName.equals(lowerPkgName)) {
+					return lowerPkgName;
+				}
+			}
 		}
 		
-		updateManifest(builder.getProject());
+		srcPath = Paths.get(projectPath.toPortableString()+"/gen");
+		srcFolder = srcPath.toFile();
+		if(srcFolder.exists() && srcFolder.isDirectory()) {
+			for(String fName : srcFolder.list()) {
+				if(fName.equals(upperPkgName)) {
+					return upperPkgName;
+				}
+				if(fName.equals(firstLower)) {
+					return firstLower;
+				}
+				if(fName.equals(lowerPkgName)) {
+					return lowerPkgName;
+				}
+			}
+		}
+		
+		LogUtils.info(logger, "The project belonging to model "+project.getName()+" does not seem to have generated code.");
+		return null;
 	}
 	
 	private void updateManifest(IProject project) {
--- org.emoflon.ibex.tgg.ide.viatra/src/org/emoflon/ibex/tgg/compiler/viatra/defaults/ViatraFilesGenerator.xtend
+++ org.emoflon.ibex.tgg.ide.viatra/src/org/emoflon/ibex/tgg/compiler/viatra/defaults/ViatraFilesGenerator.xtend
@@ -7,6 +7,17 @@ import org.moflon.tgg.mosl.tgg.TripleGraphGrammarFile
 import org.emoflon.ibex.tgg.compiler.defaults.DefaultFilesGenerator
 
 class ViatraFilesGenerator extends DefaultFilesGenerator {
+	
+	public static final String DEFAULT_REGISTRATION_HELPER = "_DefaultRegistrationHelper";
+	public static final String MODELGEN_APP = "MODELGEN_App"; 
+	public static final String SYNC_APP = "SYNC_App";
+	public static final String INITIAL_FWD_APP = "INITIAL_FWD_App";
+	public static final String INITIAL_BWD_APP = "INITIAL_BWD_App";
+	public static final String CC_APP = "CC_App";
+	public static final String CO_APP = "CO_App";
+	public static final String FWD_OPT_APP = "FWD_OPT_App";
+	public static final String BWD_OPT_APP = "BWD_OPT_App";
+	public static final String REGISTRATION_HELPER = "ViatraRegistrationHelper";
 
 	def static String generateDefaultRegHelperFile(String projectName) {
 		'''
@@ -35,27 +46,47 @@ class ViatraFilesGenerator extends DefaultFilesGenerator {
 		'''
 	}
 
-	def static String generateRegHelperFile(String projectName, TripleGraphGrammarFile tgg) {
+	def static String generateRegHelperFile(String projectName, String srcPkg, String trgPkg, String srcProject, String trgProject) {
 		'''
 			package org.emoflon.ibex.tgg.run.«MoflonUtil.lastCapitalizedSegmentOf(projectName).toLowerCase».config;
 			
 			import java.io.IOException;
 			
+			import org.eclipse.emf.ecore.EPackage;
+			import org.eclipse.emf.ecore.resource.Resource;
 			import org.eclipse.emf.ecore.resource.ResourceSet;
 			import org.emoflon.ibex.tgg.operational.csp.constraints.factories.«MoflonUtil.lastCapitalizedSegmentOf(projectName).toLowerCase».UserDefinedRuntimeTGGAttrConstraintFactory;
 			import org.emoflon.ibex.tgg.operational.defaults.IbexOptions;
 			import org.emoflon.ibex.tgg.compiler.defaults.IRegistrationHelper;
 			import org.emoflon.ibex.tgg.operational.strategies.modules.IbexExecutable;
+			import org.emoflon.ibex.tgg.operational.strategies.opt.BWD_OPT;
+			import org.emoflon.ibex.tgg.operational.strategies.opt.FWD_OPT;
 			import org.emoflon.ibex.tgg.runtime.viatra.ViatraTGGEngine;
+			
+			import «srcPkg».impl.«srcPkg.toFirstUpper»PackageImpl;
+			import «trgPkg».impl.«trgPkg.toFirstUpper»PackageImpl;
 			
 			public class ViatraRegistrationHelper implements IRegistrationHelper {
 			
 				/** Load and register source and target metamodels */
 				public void registerMetamodels(ResourceSet rs, IbexExecutable executable) throws IOException {
-					// Replace to register generated code or handle other URI-related requirements
-					«FOR imp : tgg.imports»
-						executable.getResourceHandler().loadAndRegisterMetamodel("«imp.name»");
-					«ENDFOR»
+					// Load and register source and target metamodels
+					«srcPkg.toFirstUpper»PackageImpl.init();
+					«trgPkg.toFirstUpper»PackageImpl.init();
+				
+				if(executable instanceof FWD_OPT) {
+					Resource res = executable.getResourceHandler().loadResource("platform:/resource/«trgProject»/model/«trgProject».ecore");
+					EPackage pack = (EPackage) res.getContents().get(0);
+					rs.getPackageRegistry().put("platform:/resource/«trgProject»/model/«trgProject».ecore", pack);
+					rs.getPackageRegistry().put("platform:/plugin/«trgProject»/model/«trgProject».ecore", pack);
+					}
+											
+				if(executable instanceof BWD_OPT) {
+					Resource res = executable.getResourceHandler().loadResource("platform:/resource/«srcProject»/model/«srcProject».ecore");
+					EPackage pack = (EPackage) res.getContents().get(0);
+					rs.getPackageRegistry().put("platform:/resource/«srcProject»/model/«srcProject».ecore", pack);
+					rs.getPackageRegistry().put("platform:/plugin/«srcProject»/model/«srcProject».ecore", pack);
+					}
 				}
 			
 				/** Create default options **/
