488882486df6582289a63aa875a046633f2e980f
Adrian Möller <45700082+adrianmoeller@users.noreply.github.com>
2020-07-15 10:57:10 +0200

    Match parameter are now inserted in a predefined order

--- org.emoflon.ibex.gt.viatra/src/org/emoflon/ibex/gt/viatra/runtime/ViatraGTMatch.java
+++ org.emoflon.ibex.gt.viatra/src/org/emoflon/ibex/gt/viatra/runtime/ViatraGTMatch.java
@@ -1,19 +1,34 @@
 package org.emoflon.ibex.gt.viatra.runtime;
 
+import java.util.List;
+
 import org.eclipse.viatra.query.runtime.api.IPatternMatch;
 import org.emoflon.ibex.common.operational.SimpleMatch;
+import org.emoflon.ibex.tgg.compiler.patterns.PatternSuffixes;
+import org.emoflon.ibex.tgg.operational.matches.TGGMatchParameterOrderProvider;
 
 public class ViatraGTMatch extends SimpleMatch {
 	/**
 	 * Creates a new ViatraGTMatch with the given frame and pattern.
 	 * 
-	 * @param match
-	 *            the Viatra match 
+	 * @param match the Viatra match
 	 */
 	public ViatraGTMatch(final IPatternMatch match) {
 		super(match.patternName());
-		for (String parameter : match.parameterNames()) {
-			put(parameter, match.get(parameter));
+		List<String> params = null;
+		if (match.patternName() != null)
+			params = TGGMatchParameterOrderProvider.getParams(PatternSuffixes.removeSuffix(match.patternName()));
+		if (params != null) {
+			// Insert parameters in a predefined order for determined match hashing
+			for (String p : params) {
+				Object v = match.get(p);
+				if (v != null)
+					put(p, v);
+			}
+		} else {
+			for (String parameter : match.parameterNames()) {
+				put(parameter, match.get(parameter));
+			}
 		}
 	}
 }
