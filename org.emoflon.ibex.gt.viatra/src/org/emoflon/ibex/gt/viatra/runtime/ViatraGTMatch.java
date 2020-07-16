27a23018e0c7733827b7e15ecdf04f21b54bb15b
Adrian Möller <45700082+adrianmoeller@users.noreply.github.com>
2020-07-16 19:01:12 +0200

    Fix TGG match parameter ordering

--- org.emoflon.ibex.gt.viatra/src/org/emoflon/ibex/gt/viatra/runtime/ViatraGTMatch.java
+++ org.emoflon.ibex.gt.viatra/src/org/emoflon/ibex/gt/viatra/runtime/ViatraGTMatch.java
@@ -15,19 +15,32 @@ public class ViatraGTMatch extends SimpleMatch {
 	 */
 	public ViatraGTMatch(final IPatternMatch match) {
 		super(match.patternName());
+		if (TGGMatchParameterOrderProvider.isInitialized())
+			initWithOrderedParams(match);
+		else
+			init(match);
+	}
+
+	private void init(final IPatternMatch match) {
+		for (String parameter : match.parameterNames()) {
+			put(parameter, match.get(parameter));
+		}
+	}
+
+	/**
+	 * Inserts parameters in a predefined order for determined match hashing.
+	 * 
+	 * @param match
+	 */
+	private void initWithOrderedParams(final IPatternMatch match) {
 		List<String> params = null;
 		if (match.patternName() != null)
 			params = TGGMatchParameterOrderProvider.getParams(PatternSuffixes.removeSuffix(match.patternName()));
 		if (params != null) {
-			// Insert parameters in a predefined order for determined match hashing
 			for (String p : params) {
 				Object v = match.get(p);
 				if (v != null)
 					put(p, v);
-			}
-		} else {
-			for (String parameter : match.parameterNames()) {
-				put(parameter, match.get(parameter));
 			}
 		}
 	}
