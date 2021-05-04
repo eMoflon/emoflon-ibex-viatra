cb6c911b983e85f42f42bd70954212227f3832d7
SebastianE <sebastian.ehmes@gmail.com>
2021-05-04 10:14:38 +0200

    changed viatra properties to reflect the current capabilities of our viatra wrapper properly

--- org.emoflon.ibex.gt.viatra/src/org/emoflon/ibex/gt/viatra/runtime/ViatraProperties.java
+++ org.emoflon.ibex.gt.viatra/src/org/emoflon/ibex/gt/viatra/runtime/ViatraProperties.java
@@ -10,17 +10,17 @@ public class ViatraProperties implements IPatternInterpreterProperties {
 	
 	@Override
 	public boolean supports_arithmetic_attr_constraints() {
-		return true;
+		return false;
 	}
 	
 	@Override
 	public boolean supports_count_matches() {
-		return true;
+		return false;
 	}
 	
 	@Override
 	public boolean supports_transitive_closure() {
-		return true;
+		return false;
 	}
 	
 	@Override
